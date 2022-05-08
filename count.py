6a4a79e2fc26a7e18d7d5e5ca6bfb3e8b2f463ff
aarthur0 <99750742+aarthur0@users.noreply.github.com>
2022-05-08 19:20:46 +0400

    Update count.py

--- count.py
+++ count.py
@@ -1,13 +1,9 @@
 #count
-s=input("input a string ")
-k=input("input the searched word ")
-def countt(m,n):
+def coount(m,n):
     count =0
     lst=m.split()
     for i in lst:
         if(i==n):
             count+=1
     return count
-    
-print(countt(s,k))
 
